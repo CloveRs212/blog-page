b8ea5e5545266c68abe14cb3ff93119d8589556d
CloveRs212 <cloverindustrys@gmail.com>
2023-12-10 17:14:33 -0500

    Landing Page Update v0.01

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <a href="" className="logo">CloveRs Blog Page</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+      <div className="post">
+        <div className='img'>
+          <img src='https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg' alt=''/>
+        </div>
+        <div className='texts'>
+          <h2>This is Just placeholder</h2>
+          <p>There will be additional information right here. That will give a introduction to the blog post of my the night</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className='img'>
+          <img src='https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg' alt=''/>
+        </div>
+        <div className='texts'>
+          <h2>This is Just placeholder</h2>
+          <p>There will be additional information right here. That will give a introduction to the blog post of my the night</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className='img'>
+          <img src='https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg' alt=''/>
+        </div>
+        <div className='texts'>
+          <h2>This is Just placeholder</h2>
+          <p>There will be additional information right here. That will give a introduction to the blog post of my the night</p>
+        </div>
+      </div>
+    </main>
   );
 }
 
